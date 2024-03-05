eed9c2847e742584add16ac8dcd2ae5874bc6d8a
Денис Сухарев <denis_sukharev_02@mail.ru>
2024-03-05 23:42:57 +0700

    Создание БД

--- data_collection/1_create_tables.py
+++ data_collection/1_create_tables.py
@@ -9,8 +9,10 @@ conn = psycopg2.connect(
 cursor = conn.cursor()
 
 cursor.execute('''
-               
+               DROP TABLE hotel_rating;
+               DROP TABLE hotel_stars;
                DROP TABLE poi_type;
+               DROP TABLE poi_category;
                DROP TABLE poi_coordinates;
                DROP TABLE  poi;
                DROP TABLE  district;
@@ -35,51 +37,49 @@ cursor.execute('''CREATE TABLE poi_coordinates (
                latitude DOUBLE PRECISION NOT NULL,
                longitude DOUBLE PRECISION NOT NULL,
                FOREIGN KEY (poi_id) REFERENCES poi(poi_id));''')
-
-
-# cursor.execute('''CREATE TABLE hotel_type (
-#                id SERIAL PRIMARY KEY,
-#                poi_id INTEGER NOT NULL,
-#                type VARCHAR(300) NOT NULL,
-#                FOREIGN KEY (poi_id) REFERENCES poi(poi_id));''')
-
-
 cursor.execute('''CREATE TABLE poi_type (
                id SERIAL PRIMARY KEY,
                poi_id INTEGER NOT NULL,
-               type VARCHAR(100) NOT NULL,
+               type VARCHAR(300) NOT NULL,
                FOREIGN KEY (poi_id) REFERENCES poi(poi_id));''')
-# cursor.execute('''CREATE TABLE hotel_stars (
-#                id SERIAL PRIMARY KEY,
-#                poi_id INTEGER NOT NULL,
-#                stars DOUBLE PRECISION,
-#                FOREIGN KEY (poi_id) REFERENCES poi(poi_id));''')
-# cursor.execute('''CREATE TABLE hotel_rating (
-#                id SERIAL PRIMARY KEY,
-#                poi_id INTEGER NOT NULL,
-#                rating DOUBLE PRECISION,
-#                time TIMESTAMP DEFAULT NOW(),
-#                FOREIGN KEY (poi_id) REFERENCES poi(poi_id));''')
+cursor.execute('''CREATE TABLE poi_category (
+               id SERIAL PRIMARY KEY,
+               poi_id INTEGER NOT NULL,
+               category VARCHAR(100) NOT NULL,
+               FOREIGN KEY (poi_id) REFERENCES poi(poi_id));''')
+cursor.execute('''CREATE TABLE hotel_stars (
+               id SERIAL PRIMARY KEY,
+               poi_id INTEGER NOT NULL,
+               stars DOUBLE PRECISION,
+               FOREIGN KEY (poi_id) REFERENCES poi(poi_id));''')
+cursor.execute('''CREATE TABLE hotel_rating (
+               id SERIAL PRIMARY KEY,
+               poi_id INTEGER NOT NULL,
+               rating DOUBLE PRECISION,
+               time TIMESTAMP DEFAULT NOW(),
+               FOREIGN KEY (poi_id) REFERENCES poi(poi_id));''')
 
 conn.commit()
 cursor.close()
 conn.close()
 
-# CREATE TABLE poi_time (
-#     id SERIAL PRIMARY KEY,
-#     poi_id INTEGER NOT NULL,
-#     opening_time TIME,
-#     closing_time TIME,
-#     FOREIGN KEY (poi_id) REFERENCES poi(poi_id));
 
-# CREATE TABLE poi_duration (
-#     id SERIAL PRIMARY KEY,
-#     poi_id INTEGER NOT NULL,
-#     duration INTEGER,
-#     FOREIGN KEY (poi_id) REFERENCES poi(poi_id));
 
-# CREATE TABLE hotel_price (
-#     id SERIAL PRIMARY KEY,
-#     poi_id INTEGER NOT NULL,
-#     price INTEGER,
-#     FOREIGN KEY (poi_id) REFERENCES poi(poi_id));
+'''CREATE TABLE poi_time (
+    id SERIAL PRIMARY KEY,
+    poi_id INTEGER NOT NULL,
+    opening_time TIME,
+    closing_time TIME,
+    FOREIGN KEY (poi_id) REFERENCES poi(poi_id));
+
+CREATE TABLE poi_duration (
+    id SERIAL PRIMARY KEY,
+    poi_id INTEGER NOT NULL,
+    duration INTEGER,
+    FOREIGN KEY (poi_id) REFERENCES poi(poi_id));
+
+CREATE TABLE hotel_price (
+    id SERIAL PRIMARY KEY,
+    poi_id INTEGER NOT NULL,
+    price INTEGER,
+    FOREIGN KEY (poi_id) REFERENCES poi(poi_id));'''
