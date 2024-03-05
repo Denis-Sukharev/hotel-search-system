03f7cc7e8a496812b96fee2922f8357e83a55283
Денис Сухарев <denis_sukharev_02@mail.ru>
2024-03-05 23:42:21 +0700

    Создание БД

--- data_collection/2_get_hotels.py
+++ data_collection/2_get_hotels.py
@@ -50,9 +50,21 @@ def add_coordinates_to_db(poi_id, latitude, longitude):
     conn.commit()
     cursor.execute('''INSERT INTO poi_coordinates (poi_id, latitude, longitude) VALUES (%s, %s, %s);''', (poi_id, latitude, longitude))
 
-def add_poi_type_to_db(poi_id, type_name):
-    cursor.execute('''INSERT INTO poi_type (poi_id, type) VALUES (%s, %s);''', (poi_id, type_name))
+def add_poi_category_to_db(poi_id, category_name):
+    cursor.execute('''INSERT INTO poi_category (poi_id, category) VALUES (%s, %s);''', (poi_id, category_name))
     conn.commit()
+
+def add_poi_type_to_db(poi_id, poi_type):
+    cursor.execute('''INSERT INTO poi_type (poi_id, type) VALUES (%s, %s);''', (poi_id, poi_type))
+    conn.commit()
+
+def add_hotel_rating_to_db(poi_id, rating):
+    cursor.execute('''INSERT INTO hotel_rating (poi_id, rating) VALUES (%s, %s);''', (poi_id, rating))
+    conn.commit()
+
+def add_hotel_stars_to_db(poi_id, stars):
+    cursor.execute('''INSERT INTO hotel_stars (poi_id, stars) VALUES (%s, %s);''', (poi_id, stars))
+    conn.commit()  
 
 def get_district(latitude, longitude):
     geolocator = Nominatim(user_agent="geoapiExercises")
@@ -80,17 +92,25 @@ s = BeautifulSoup(html.content, 'html.parser')
 results = s.find(id='hotels-list')
 hotel_links = results.find_all('a', class_='link__trigger')
 
-for link in hotel_links[:50]:
+for link in hotel_links[:3]:
     href = link['href']
     new_url = f'https://m.101hotels.com{href}#map'
-    
+
     pause = random.uniform(1,2)
     time.sleep(pause)
     
     new_html = requests.get(new_url)
-    hotel_name = link.find(class_='link__title').get_text(strip=True) 
-    coord = re.search(r'center:\s\[(\d+\.\d+)\,(\d+\.\d+)\]', new_html.text)
+    hotel_name = link.find(class_='link__title').get_text(strip=True)
+    poi_type = link.find(class_='link__description').get_text(strip=True)
+        
+    rating_match = re.search(r'<span class="hotel-rating-item__rating-number">(\d+\.\d+)</span>', new_html.text)
+    rating = rating_match.group(1)
+
+    stars_match = re.search(r'<div class="hotel__stars">(.+)</div>', new_html.text)
+    stars_string = stars_match.group(1) if stars_match else ''
+    stars_count = stars_string.count('icon icon_star colored')
     
+    coord = re.search(r'center:\s\[(\d+\.\d+)\,(\d+\.\d+)\]', new_html.text)
     if coord:
         lat, lon = coord.groups()
         district = get_district(lat, lon)
@@ -99,8 +119,13 @@ for link in hotel_links[:50]:
             district_id = add_district_to_db(city_id, district)
             poi_id = add_poi_to_db(district_id, hotel_name)
             add_coordinates_to_db(poi_id, lat, lon)
-            add_poi_type_to_db(poi_id, 'residence')
+            add_poi_category_to_db(poi_id, 'residence')
+            add_poi_type_to_db(poi_id, poi_type)
+            add_hotel_rating_to_db(poi_id, rating)
             
+            if stars_count > 0:
+                add_hotel_stars_to_db(poi_id, stars_count)
+
             print(f"УСПЕШНО: {hotel_name}")
        
         else:
