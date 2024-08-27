bd027d9fb119265e18fe47795dbdc0801ec22ac4
losos3000 <golikov.andrei.2001@yandex.ru>
2024-08-27 10:19:23 +0700

    docker

--- server/main.py
+++ server/main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     # HTTPSRedirectMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=["http://localhost:5173/", "http://127.0.0.1:5173/", "http://172.17.3.34:8080/"],
     allow_credentials=True,
     allow_methods=["GET", "POST", "OPTIONS"],
     allow_headers=[
